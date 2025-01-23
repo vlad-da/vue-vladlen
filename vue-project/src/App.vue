d9d15baa8fe7003ea19a9677ade1bdc158ade8de
Vlasenko Vladislav <vl.vlasenko@mail.astondevs.ru>
2025-01-23 23:04:24 +0300

    Добавляет инпуты

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,77 @@
 <template>
-  <header>
-    <TheHeader />
-  </header>
+  <div class="container">
+    <form class="card" @submit.prevent="submitForm">
+      <h1>Анкета на Vue разработчика!</h1>
+      <div class="form-control">
+        <label for="name">Как тебя зовут?</label>
+        <input v-model="name" type="text" id="name" placeholder="Введи имя">
+      </div>
+
+      <div class="form-control">
+        <label for="age">Выбери возраст</label>
+        <input v-model.number="age" type="number" id="age" value="20">
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label>
+        <select v-model="city" id="city">
+          <option value="spb">Санкт-Петербург</option>
+          <option value="msk">Москва</option>
+          <option value="kzn">Казань</option>
+          <option value="nsk">Новосибирск</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Готов к переезду в Токио?</span>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" v-model="goTo" value="yes"/> Да</label>
+        </div>
+
+        <div class="checkbox">
+          <label><input type="radio" name="trip" v-model="goTo" value="no"/> Нет</label>
+        </div>
+      </div>
+
+      <div class="form-checkbox">
+        <span class="label">Что знаешь во Vue?</span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="checks" name='country' value="japan"/> Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="checks" name='country' value="russia"/> Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="checks" name='country' value="usa"/> Vue Router</label>
+        </div>
+      </div>
+
+      <button type="submit" class="btn primary">Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
-export default {
-  name: 'App',
-  components: {
-    TheHeader,
-  },
-}
-</script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+export default {
+  data() {
+    return {
+      name: '',
+      age: 22,
+      city: 'spb',
+      goTo: null,
+      checks: []
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log(this.name);
+      console.log(this.age);
+      console.log(this.city);
+      console.log(this.goTo);
+      console.log(this.checks);
+    }
+  }
 }
-</style>
+
+</script>
