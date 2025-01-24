786f4e51799c47823621189eda935ced2acbea78
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2025-01-24 16:40:22 +0300

    Работа с бэком

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,77 +1,83 @@
 <template>
   <div class="container">
-    <form class="card" @submit.prevent="submitForm">
-      <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control">
-        <label for="name">Как тебя зовут?</label>
-        <input v-model="name" type="text" id="name" placeholder="Введи имя">
-      </div>
+    <form class="card" @submit.prevent="createPearson">
+      <h2>Работа с базой данных</h2>
 
       <div class="form-control">
-        <label for="age">Выбери возраст</label>
-        <input v-model.number="age" type="number" id="age" value="20">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name" />
       </div>
 
-      <div class="form-control">
-        <label for="city">Твой город</label>
-        <select v-model="city" id="city">
-          <option value="spb">Санкт-Петербург</option>
-          <option value="msk">Москва</option>
-          <option value="kzn">Казань</option>
-          <option value="nsk">Новосибирск</option>
-        </select>
-      </div>
-
-      <div class="form-checkbox">
-        <span class="label">Готов к переезду в Токио?</span>
-        <div class="checkbox">
-          <label><input type="radio" name="trip" v-model="goTo" value="yes"/> Да</label>
-        </div>
-
-        <div class="checkbox">
-          <label><input type="radio" name="trip" v-model="goTo" value="no"/> Нет</label>
-        </div>
-      </div>
-
-      <div class="form-checkbox">
-        <span class="label">Что знаешь во Vue?</span>
-        <div class="checkbox">
-          <label><input type="checkbox" v-model="checks" name='country' value="japan"/> Vuex</label>
-        </div>
-        <div class="checkbox">
-          <label><input type="checkbox" v-model="checks" name='country' value="russia"/> Vue CLI</label>
-        </div>
-        <div class="checkbox">
-          <label><input type="checkbox" v-model="checks" name='country' value="usa"/> Vue Router</label>
-        </div>
-      </div>
-
-      <button type="submit" class="btn primary">Отправить</button>
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
     </form>
+    <AppLoader v-if="load" />
+    <AppList v-else :people="people" @load-list="loadPeople" @delete-pearson="deletPearson" />
   </div>
 </template>
 
 <script>
-
+import AppList from './AppList.vue'
+import axios from 'axios'
+import AppLoader from './AppLoader.vue'
 export default {
   data() {
     return {
       name: '',
-      age: 22,
-      city: 'spb',
-      goTo: null,
-      checks: []
+      people: [],
+      load: false,
     }
   },
+  components: {
+    AppList,
+    AppLoader,
+  },
+  mounted() {
+    this.loadPeople()
+  },
   methods: {
-    submitForm() {
-      console.log(this.name);
-      console.log(this.age);
-      console.log(this.city);
-      console.log(this.goTo);
-      console.log(this.checks);
-    }
-  }
-}
+    async createPearson() {
+      const res = await fetch(
+        ' https://vueproject-5ecf6-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            firstName: this.name,
+          }),
+        },
+      )
+      const fire = await res.json()
+      this.people.push({ firstName: this.name, id: fire.name })
+      this.name = ''
+    },
 
+    async loadPeople() {
+      this.load = true
+
+      const { data } = await axios.get(
+        'https://vueproject-5ecf6-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+      )
+      const res = Object.keys(data).map((key) => {
+        return {
+          id: key,
+          firstName: data[key].firstName,
+        }
+      })
+      console.log(res)
+      this.people = res
+      this.load = false
+    },
+
+    async deletPearson(id) {
+      await axios.delete(
+        `https://vueproject-5ecf6-default-rtdb.europe-west1.firebasedatabase.app/people/${id}.json`,
+      )
+      this.people = this.people.filter((pers) => pers.id !== id)
+    },
+  },
+}
 </script>
+
+<style></style>
---
+++ vue-project/src/AppLoader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="loader">Loading...</div>
+</template>
+
+<script>
+export default {
+  name: 'AppLoader',
+}
+</script>
